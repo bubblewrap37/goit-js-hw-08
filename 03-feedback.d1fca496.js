fd94e588bca49d808b01762d7550cc971a79b51d
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-05-05 19:52:51 +0000

    Deploying to gh-pages from @ bubblewrap37/goit-js-hw-08@68eb9a50a50a92b11924ac80793b9b8fdf4d219e 🚀

---
+++ 03-feedback.d1fca496.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector(".feedback-form"),t=JSON.parse(localStorage.getItem("feedback-form-state"));e.elements.email.value=t.email,e.elements.message.value=t.message,e.addEventListener("input",function(e){let t=JSON.stringify({email:e.currentTarget.elements.email.value,message:e.currentTarget.elements.message.value});localStorage.setItem("feedback-form-state",t)})}();
+//# sourceMappingURL=03-feedback.d1fca496.js.map
